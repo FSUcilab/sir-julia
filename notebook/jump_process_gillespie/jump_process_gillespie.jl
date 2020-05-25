e40b5c83b2f6056c85f5bccefa993a0a65a1ee7f
erlebach <gordon.erlebach@gmail.com>
2020-05-25 15:27:53 -0400

    Added two folders (Gillespie and Diffeqbio)
    These two folders were copy from tutorials/ and transformed to
    notebooks.
    
    Added some comments to the other files, prepended by GE

---
+++ notebook/jump_process_gillespie/jump_process_gillespie.jl
@@ -0,0 +1,55 @@
+
+using Pkg
+Pkg.add(PackageSpec(url="https://github.com/sdwfrost/Gillespie.jl", rev="master"))
+
+
+using Gillespie
+using Random
+using StatsPlots
+using BenchmarkTools
+
+
+function sir_rates(x,parms)
+  (S,I,R) = x
+  (β,c,γ) = parms
+  N = S+I+R
+  infection = β*c*I/N*S
+  recovery = γ*I
+  [infection,recovery]
+end;
+
+
+sir_transitions = [[-1 1 0];[0 -1 1]];
+
+
+tmax = 40.0;
+
+
+u0 = [990,10,0]; # S,I,R
+
+
+p = [0.05,10.0,0.25]; # β,c,γ
+
+
+Random.seed!(1234);
+
+
+sol_jump = ssa(u0,sir_rates,sir_transitions,p,tmax);
+
+
+df_jump = ssa_data(sol_jump);
+
+
+@df df_jump plot(:time,
+    [:x1 :x2 :x3],
+    label=["S" "I" "R"],
+    xlabel="Time",
+    ylabel="Number")
+
+
+@benchmark ssa(u0,sir_rates,sir_transitions,p,tmax)
+
+
+include(joinpath(@__DIR__,"tutorials","appendix.jl"))
+appendix()
+
